4816e198224929be83ff087455ceec559db6419a
Guill Lo <guill.bout@gmail.com>
2020-07-23 20:57:10 +0800

    first reconversion from dash-docsets repo

---
+++ tests/Unit/PloiTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Docsets\Ploi;
+use Godbout\DashDocsetBuilder\Services\DocsetBuilder;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class PloiTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->docset = new Ploi();
+        $this->builder = new DocsetBuilder($this->docset);
+    }
+
+    /** @test */
+    public function it_generates_a_table_of_contents()
+    {
+        $toc = $this->docset->entries(
+            $this->docset->downloadedIndex()
+        );
+
+        $this->assertNotEmpty($toc);
+    }
+
+    /** @test */
+    public function it_can_format_the_documentation_files()
+    {
+        $header = '<header';
+
+        $this->assertStringContainsString(
+            $header,
+            Storage::get($this->docset->downloadedIndex())
+        );
+
+        $this->assertStringNotContainsString(
+            $header,
+            $this->docset->format(
+                $this->docset->downloadedIndex()
+            )
+        );
+    }
+}
